51122a7f6f272cf1d8911b495fe149f1395d3886
clairemuiru1 <muiruclaire@gmail.com>
2023-10-17 21:48:22 +0300

    "Commit changes"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+fetch("http://localhost:3000/dogs")
+.then(response => response.json())
+.then((json) => showDogs);
 
+function showDogs(data) {
+    dogs.forEach(dog => {
+        Name.textcontext= dog.name;
+        Breed.textcontext=dog.breed;
+        sex.textcontext=dog.sex;
+    });
+  }
 })
